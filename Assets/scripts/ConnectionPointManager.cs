528fd0b31cf0700b23c0700894e597c086ea2f11
Otto Uusihakala <otto.uusihakala@gmail.com>
2015-11-03 19:34:41 +0200

    Connection Point Manager, contains references to "block's" beginning point and end point. For use in the faux random level generation.

---
+++ Assets/scripts/ConnectionPointManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectionPointManager : MonoBehaviour {
+
+    public Transform beginPoint;
+    public Transform endPoint;
+
+	// Use this for initialization
+	void Start () {
+        Transform[] points = GetComponentsInChildren<Transform>();
+        foreach (Transform point in points)
+        {
+            if (point.tag.Equals("Begin Point"))
+            {
+                beginPoint = point;
+            }
+            if (point.tag.Equals("End Point"))
+            {
+                endPoint = point;
+            }
+        }
+	}
+}
